cd63ea58af3420e3d99d6ba7a6cb6678600f0f56
markoperak <marko.perak50@gmail.com>
2025-03-27 15:17:31 +0100

    Delete zad1.py

--- zad1.py
+++
@@ -1,9 +0,0 @@
-def total_euro(radni_sati, satnica):
-    return radni_sati*satnica
-
-print("Unesite broj radnih sati: ")
-radni_sati = float(input())
-print("Unesite satnicu: ")
-satnica = float(input())
-
-print(f"Ukupno: {total_euro(radni_sati, satnica)}")
